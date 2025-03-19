24ce6d6b1d6013b6b4d0a96002443ff69c257802
JoseVargasTR <Jose.Vargas@thomsonreuters.com>
2025-03-19 17:54:23 -0600

    changesdf

--- iac/cdk/lambda-cdk.ts
+++ iac/cdk/lambda-cdk.ts
@@ -1,6 +1,5 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-import * as path from 'path';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as s3 from 'aws-cdk-lib/aws-s3';
 
